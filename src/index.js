b705e290eb071bc58e52914798968ddbcb10e196
Glemakk <kovgar.brand@gmail.com>
2021-05-30 16:58:29 +0200

    Finished with large size Img

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@ import './js/api';
 import './js/fetchImgCards';
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
+
+const galleryContainer = document.querySelector('.js-gallery-container');
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+  const imgEl = event.target.classList.contains('img-element');
+  if (!imgEl) {
+    return;
+  }
+  //   console.log(event.target.dataset.largesize);
+  const largeImg = event.target.dataset.largesize;
+  const instance = basicLightbox.create(`
+    <img src=${largeImg}>
+`);
+
+  instance.show();
+}
